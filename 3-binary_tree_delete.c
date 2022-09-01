1e3336e78fbdfc0231888a3cf24c16bb1f741de9
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-01 13:33:52 -0700

    try 2

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - dletes an entire binary tree
+ *
+ * @tree: pointer to the root of node of the tree
+ *
+ * Return: nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	/* Chequea lo que pide Holberton-sama */
 	if (tree == NULL)
 	{
 		return;
 	}
 
+	/* deletea como izuqerda, despues derecha y por ultimo el tree */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
