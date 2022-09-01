bb98287baf0b662d02354cd18514f11c6579b6f9
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-01 15:24:50 -0700

    try1

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/** 
+ * binary_tree_is_root - checks if a given node is a root
+ *
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if root, 0 if fail
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	/*Por que parent?, bueno pide saber si es root, osea el orignal,
+	 * el primero de los primeros, el unico entre los todos...
+	 *
+	 * Entonces lo que hacemos es ver si node tiene parent, si tiene
+	 * quiere decir que no es root y tiene un pariente, y si no tiene
+	 * es que es root */
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
