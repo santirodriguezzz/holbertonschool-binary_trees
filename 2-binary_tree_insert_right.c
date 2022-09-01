82e254bd6eb477d8b055ccfcfcdfdbd7b2e3ff1a
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-01 15:49:54 -0700

    betyy

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,6 +8,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *temp;
+
 	if (parent == NULL)
 		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
