627a728bb6e46ed66d8a7e5de3fd178087a4ae2f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-01 15:34:06 -0700

    bettyyyyyyyyyyyyy

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/** 
+/**
  * binary_tree_is_root - checks if a given node is a root
  *
  * @node: pointer to the node to check
@@ -13,7 +13,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	/* 
+	/*
 	 * Por que parent?, bueno pide saber si es root, osea el orignal,
 	 * el primero de los primeros, el unico entre los todos...
 	 *
