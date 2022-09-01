e8f660601bd08c317f01c75fec12ac17cb8ed43c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-01 13:13:42 -0700

    try 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
-		parent->left = node;
+		parent->left->parent = node;
 	}
 
 	parent->left = node;
