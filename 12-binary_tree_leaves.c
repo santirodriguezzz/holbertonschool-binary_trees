0c39754c24744bf2160bd750f0eeef8f69610d1d
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-04 13:16:13 -0700

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return(1);
+		return (1);
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
 	return (left + right);
