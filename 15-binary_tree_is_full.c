e4dc8239cdde62bb074e2217c7ffe9b07ba7c055
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-04 14:54:30 -0700

    ahhh los ;

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -43,7 +43,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	L1 = binary_tree_is_full(tree->left);
 	R1 = binary_tree_is_full(tree->right);
 
-	if (L1 == 1 && R1 == 1);
+	if (L1 == 1 && R1 == 1)
 	{
 		return (1);
 	}
